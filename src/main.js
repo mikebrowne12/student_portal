cabedbd5f92e6ea9b206d1c04bc5e1de7473dbd3
Mike Browne <mikebrowne12@gmail.com>
2019-04-22 18:39:53 -0500

    Initial Push Student Portal

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import axios from "axios"; 
 
-Vue.config.productionTip = false
+axios.default.baseURL =
+  process.env.NODE_NEV === "development" ? "http://localhost:3000" : "/"; 
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
