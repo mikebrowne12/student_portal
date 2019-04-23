f3f988ed25561cbe01b0d6cd5b141e005ed62083
brandonaray <brandonray1225@gmail.com>
2019-04-22 21:03:21 -0500

    add student information to show

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,37 @@
 <template>
-  <div class="home"></div>
+  <div class="container">
+    <h1>{{ student.first_name }} {{ student.last_name }}</h1>
+
+    <p>{{ student.email }}</p>
+    <p>{{ student.phone_number }}</p>
+    <p>{{ student.short_bio }}</p>
+    <p>{{ student.linkedin_url }}</p>
+    <p>{{ student.twitter_handle }}</p>
+    <p>{{ student.personal_website }}</p>
+    <p>{{ student.online_resume_url }}</p>
+    <p>{{ student.github_url }}</p>
+  </div>
 </template>
 
 <script>
 export default {
   name: "home",
-  components: {}
+  components: {},
+  data: function() {
+    return {
+      student: {
+        first_name: "Brandon",
+        last_name: "Ray",
+        email: "brandon@email.com",
+        phone_number: "555-555-5555",
+        short_bio: "Brandon is a student who studies.",
+        linkedin_url: "linkedin.com/brandon",
+        twitter_handle: "@brandon",
+        personal_website: "www.brandonray.com",
+        online_resume_url: "placeholder",
+        github_url: "github.com/brandon"
+      }
+    };
+  }
 };
 </script>
