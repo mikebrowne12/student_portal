ac3e84fadf2e59141652de47d060d8569ae7428a
elizabethcruz8 <elicruz92.10@gmail.com>
2019-04-23 20:13:41 -0500

    Add all models in home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,11 +10,40 @@
       </p>
       <button v-on:click="editSection('info')">Edit</button>
     </div>
-    <div class="experience"></div>
-    <h1></h1>
-    <div class="education"></div>
-    <div class="skills"></div>
-    <div class="capstone"></div>
+
+    <h1>Experience</h1>
+    <div v-for="experience in student.experiences">
+      <p>{{ experience.job_title }}</p>
+      <p>{{ experience.company }}</p>
+      <p>{{ experience.start_date }}</p>
+      <p>{{ experience.end_date }}</p>
+      <p>{{ experience.details }}</p>
+      <button v-on:click="editSection('info')">Edit</button>
+    </div>
+
+    <h1>Education</h1>
+    <div v-for="education in student.educations">
+      <p>{{ education.university_name }}</p>
+      <p>{{ education.degree }}</p>
+      <p>{{ education.start_date }}</p>
+      <p>{{ education.end_date }}</p>
+      <p>{{ education.details }}</p>
+      <button v-on:click="editSection('info')">Edit</button>
+    </div>
+
+    <h2>Skills</h2>
+    <div v-for="skill in student.skills">
+      <p>{{ skill.skill_name }}</p>
+      <button v-on:click="editSection('info')">Edit</button>
+    </div>
+
+    <h2>Capstone</h2>
+    <div v-for="capstone in student.capstones">
+      <p>{{ capstone.description }}</p>
+      <p>{{ capstone.project_url }}</p>
+      <p>{{ capstone.screenshot_url }}</p>
+      <button v-on:click="editSection('info')">Edit</button>
+    </div>
   </div>
 </template>
 
@@ -34,7 +63,44 @@ export default {
         twitter_handle: "@brandon",
         personal_website: "www.brandonray.com",
         online_resume_url: "placeholder",
-        github_url: "github.com/brandon"
+        github_url: "github.com/brandon",
+        experiences: [
+          {
+            job_title: "Manager",
+            company: "Actualize",
+            start_date: "01/01/01-",
+            end_date: "Present",
+            details: " Manages Actualize employees."
+          },
+          {
+            job_title: "Fry cook",
+            company: "KFC",
+            start_date: "01/01/98 -",
+            end_date: "04/02/00",
+            details: "Fried Chicken."
+          }
+        ],
+        educations: [
+          {
+            university_name: "Harvard",
+            degree: "Bachelor in Economics",
+            start_date: "03/01/94 -",
+            end_date: "06/01/96",
+            details: "Cum laude"
+          }
+        ],
+        skills: [
+          {
+            skill_name: "ProgresSQL, Ruby, Rails, Vue.js, Git, Github "
+          }
+        ],
+        capstones: [
+          {
+            description: "Actualize Website",
+            project_url: "www.actualize.org",
+            screenshot_url: ""
+          }
+        ]
       }
     };
   },
