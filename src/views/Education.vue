7b86d7a2a02fbee18f5a188539aaf1d6c1f5392f
Mike Browne <mikebrowne12@gmail.com>
2019-04-23 20:31:08 -0500

    Edit Capstone and Education Vue

--- src/views/Education.vue
+++ src/views/Education.vue
@@ -2,20 +2,29 @@
   <div class="education-edit">
     <div>
       <h4>Edit Education Credentials</h4>
-      Start Date:
-      <input type="text" v-model="education.start_date" />
-      End Date:
-      <input type="text" v-model="education.end_date" />
-      Degree:
-      <input type="text" v-model="education.degree" />
-      University:
-      <input type="text" v-model="education.university_name" />
-      Details:
-      <input type="text" v-model="education.details" />
-      <button v-on:click="createEducation(education)">Create</button>
-      <button v-on:click="updateEducation(education)">Update Education</button>
-      <button v-on:click="destroyEducation(education)">Delete</button>
-      <router-link to="/">Back</router-link>
+      <button v-on:click="createEducation()">Add Education</button>
+      <div v-for="education in education">
+        <h3 class="university">{{ education.university_name }}</h3>
+        <p>Start Date: {{ education.start_date }} 
+        <p>End Date: {{ education.end_date }}</p>
+        <p>Degree: {{ education.degree }}</p>
+        <p>Details: {{ education.details }}</p>
+        <div v-if="!updateHidden">
+          Start Date:
+          <input type="text" v-model="education.start_date" />
+          End Date:
+          <input type="text" v-model="education.end_date" />
+          Degree:
+          <input type="text" v-model="education.degree" />
+          University:
+          <input type="text" v-model="education.university_name" />
+          Details:
+          <input type="text" v-model="education.details" />
+          <button v-on:click="updateEducation(education)">Update Education</button>
+          <button v-on:click="destroyEducation(education)">Delete</button>
+          <router-link to="/">Back</router-link>
+        </div>
+      </div> 
     </div>
   </div>
 </template>
@@ -26,17 +35,21 @@ import axios from "axios";
 export default {
   data: function() {
     return {
-      education: {}, 
+      education: [
+        {
+          start_date: "August 6, 2013",
+          end_date: "May 5, 2017", 
+          degree: "Chemistry", 
+          university_name: "University of Actualize",
+          details: "Graduated Summa Cum Laude", 
+        }
+      ], 
       newEducationStartDate: "",
       newEducationEndDate: "",
       newEducationDegree: "",
       newEducationUniversityName: "",
       newEducationDetails: "",
-      start_date: "August 6, 2013",
-      end_date: "May 5, 2017", 
-      degree: "Chemistry", 
-      university_name: "University of Actualize",
-      details: "Graduated Summa Cum Laude"
+      updateHidden: false, 
 
     };
   },
